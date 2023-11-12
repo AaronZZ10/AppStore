b688aee8a38595f37aadff4817d134822d02b4e9
Aaron Liu <94769848+AaronZZ10@users.noreply.github.com>
2023-11-12 05:27:15 -0600

    Add files via upload

---
+++ constants/constant.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.128.0.2:9200"
+    ES_USERNAME = "admin"
+    ES_PASSWORD = "Password"
+    STRIPE_API_KEY = "sk_test_51O4x6gHffU5e060XD8dknHlenAEQ51wdkaRKyeOCHfNJSyXyEnGpAIuiDNQ66EBby6e4PBrINuRgLBnKDM3RlmkU007yMVaUir"
+    GCS_BUCKET = "appstore-aar"
+)
